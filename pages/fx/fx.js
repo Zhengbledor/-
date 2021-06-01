1786ddff6a2e7370ed84fe463bcbddb9c757ffe4
ga892625241 <gazhjky@qq.com>
2021-06-02 02:09:09 +0800

    漫画功能完成

--- pages/fx/fx.js
+++ pages/fx/fx.js
@@ -5,16 +5,52 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        index:0,
+        manhua:[{
+            fileID:"cloud://cloud1-7g971elu4fe9349d.636c-cloud1-7g971elu4fe9349d-1305747814/漫画/WechatIMG2.jpeg"
+        },{
+            fileID:"cloud://cloud1-7g971elu4fe9349d.636c-cloud1-7g971elu4fe9349d-1305747814/漫画/WechatIMG3.jpeg"
+        },{
+            fileID:"cloud://cloud1-7g971elu4fe9349d.636c-cloud1-7g971elu4fe9349d-1305747814/漫画/WechatIMG4.jpeg"
+        },{
+            fileID:"cloud://cloud1-7g971elu4fe9349d.636c-cloud1-7g971elu4fe9349d-1305747814/漫画/WechatIMG5.jpeg"
+        },
+            ],
+        manhuaURL:[]
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        //先获得manhua的URL
+        this.getManhuaURL();
     },
-
+    getManhuaURL:function(){
+        wx.cloud.getTempFileURL({
+            fileList: this.data.manhua,
+            success: res => {
+                console.log(11)
+                var tempImaURL=this.data.manhuaURL
+                for(var i=0;i<this.data.manhua.length;i++){
+                    tempImaURL.push(res.fileList[i].tempFileURL)
+                }
+                this.setData({ 
+                manhuaURL:tempImaURL});
+              },
+              fail: console.error
+        })
+    },
+    prev:function(){
+        this.setData({
+            index:this.data.index-1
+        })
+    },
+    next:function(){
+        this.setData({
+            index:this.data.index+1
+        })
+    },
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
