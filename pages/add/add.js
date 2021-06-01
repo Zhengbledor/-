913a77cd794ae7f7d0080a79b201d2072f322858
ga892625241 <gazhjky@qq.com>
2021-06-02 01:35:51 +0800

    上传图片功能完成（不知道还有没有bug）

--- pages/add/add.js
+++ pages/add/add.js
@@ -22,6 +22,7 @@ Page({
         test:"",
         lessTest:"",
         ima:[],
+        fileList2:[],
         needLess:false
     },
     titleChange:function(e){
@@ -38,7 +39,6 @@ Page({
         console.log(e);
     },
     saveClick:function(e){
-        console.log(this.data.test)
         if(this.data.title==""){
              $wuxToast().show({
                 type:'forbidden',
@@ -57,10 +57,24 @@ Page({
             color:'#ffff',
             text:"请输入内容",
             success:()=>console.log("禁止操作")
-        })
-        return
+            })
+            return
         }
-       
+        const fileList2=this.data.fileList2
+        for(var i=0;i<fileList2.length;i++){
+            if(fileList2[i].status=="uploading")
+            {
+                $wuxToast().show({
+                    type:'forbidden',
+                    duration:1500,
+                    color:'#ffff',
+                    text:"正在上传图片",
+                    success:()=>console.log("禁止操作")
+                    })
+                return ;
+            }
+        }
+
         if(this.data.test.length>50)
         {
             var tempTest=this.data.test.substring(0,50)
@@ -74,19 +88,84 @@ Page({
             test:this.data.test,
             time:util.formatTime(new Date()),
             needLess:this.data.needLess,
-            lessTest:this.data.lessTest
+            lessTest:this.data.lessTest,
+            ima:this.data.ima
         })
         wx.setStorageSync('dairyList',dairyList)
         wx.navigateBack()
     },
     afterRead(event) {
+        const upIndex=event.detail.index
         const file  = event.detail.file;
         // 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
-        const ima = this.data.ima;
-        fileList2.push({})
-        fileList2[0].status='uploading'
+        const fileList2 = this.data.fileList2;
+        fileList2.push({
+            status:"uploading",
+            url:file.url
+        })
         this.setData({ fileList2 });
-        this.uploadImage(file.url);
+        this.uploadImag(file.url,upIndex);
+    },
+    //点击X号
+    deleteImg(event) {
+        // 页面删除（假删除）
+        const delIndex = event.detail.index
+        const { fileList2 } = this.data
+        fileList2.splice(delIndex, 1)
+        var img=this.data.ima
+        img.splice(delIndex,1)
+        this.setData({
+          fileList2:fileList2,
+          ima:img
+        })
+        // 云存储删除（真删除）
+        var fileID = ""+this.data.ima[delIndex];
+        this.deleteCloudSave(fileID)
+    
+      },
+      deleteCloudSave(fileID) {
+        console.log(fileID)
+        wx.cloud.deleteFile({
+        fileList: [fileID],
+        success: res => {
+          // handle success
+          console.log(res.fileList)
+        },
+        fail: console.error
+        })
+      },
+      //上传到云开发存储中
+    uploadImag(fileURL,upIndex){
+        var that = this
+        wx.cloud.uploadFile({
+            cloudPath:new Date().getTime()+'.png',//路径
+            filePath:fileURL,
+            success:res=>{
+                that.addImagePath(res.fileID,upIndex)//获得上传成功后的url路径
+                var img=that.data.ima;
+                img.push({//保存fileID
+                    fileID:res.fileID
+                })
+                this.setData({
+                    ima:img
+                })
+            },
+            fail: console.error
+        })
+    },
+    addImagePath(fileId,upIndex) {
+        var that = this
+        wx.cloud.getTempFileURL({
+          fileList: [fileId],
+          success: res => {
+            var url = res.fileList[0].tempFileURL
+            var fileList2=that.data.fileList2
+            fileList2[upIndex].url= url
+            fileList2[upIndex].status= 'done'
+            that.setData({ fileList2 :fileList2});
+          },
+          fail: console.error
+        })
       },
     /**
      * 生命周期函数--监听页面加载
--- pages/show/show.js
+++ pages/show/show.js
@@ -17,19 +17,30 @@ Page({
         edi:"编辑",
         lessTest:"",
         ima:[],
+        tempima:[],//暂存的图片ID
+        tempImaURL:[],//暂存的图片地址，用来在编辑的时候显示
+        imaURL:[],
         needLess:false
     },
     ediClick:function(e){
-        if(this.data.ifEdi==false)
+        if(this.data.ifEdi==false){
             this.setData({
                 ifEdi:true,
                 edi:"取消编辑"
             })
-        else
+        }
+            
+        else{
+            var tempima=Object.create(this.data.ima)
+            var tempImaURL=Object.create(this.data.imaURL)
             this.setData({
+                tempima:tempima,//回到初始位置
+                tempImaURL:tempImaURL,
                 ifEdi:false,
                 edi:"编辑"
             })
+        }
+            
     },
     delClick:function(e){
         var dl=dairyList
@@ -72,7 +83,20 @@ Page({
             })
             return
         }
-        
+        const imaURL=this.data.imaURL
+        for(var i=0;i<imaURL.length;i++){
+            if(imaURL[i].status=="uploading")
+            {
+                $wuxToast().show({
+                    type:'forbidden',
+                    duration:1500,
+                    color:'#ffff',
+                    text:"正在上传图片",
+                    success:()=>console.log("禁止操作")
+                    })
+                return ;
+            }
+        }
         if(this.data.tempTest.length>50)
         {
             var temp=this.data.tempTest.substring(0,50)
@@ -89,7 +113,7 @@ Page({
         dairyList[index].time=util.formatTime(new Date())
         dairyList[index].needLess=this.data.needLess
         dairyList[index].lessTest=this.data.lessTest
-        
+        dairyList[index].ima=this.data.tempima
         wx.setStorageSync('dairyList',dairyList)
         wx.navigateBack({
           delta: 1,
@@ -99,7 +123,6 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        console.log(options)
         var dairyList = wx.getStorageSync('dairyList')||[]
         var id=options.index
         this.setData({
@@ -107,10 +130,123 @@ Page({
             title:dairyList[id].title,
             test:dairyList[id].test,
             tempTitle:dairyList[id].title,
-            tempTest:dairyList[id].test
+            tempTest:dairyList[id].test,
+            ima:dairyList[id].ima
+        })
+        var tempima=this.data.tempima
+        for(var i=0;i<this.data.ima.length;i++){
+            tempima.push({
+                fileID:this.data.ima[i].fileID
+            })
+        }
+        this.setData({
+            tempima:tempima
+        })
+        this.getImaURL();
+    },
+    getImaURL:function(){
+        if(this.data.ima.length==undefined)
+            return
+        wx.cloud.getTempFileURL({
+            fileList: this.data.ima,
+            success: res => {
+                var tempURL=this.data.imaURL;
+                var tempImaURL=this.data.tempImaURL
+                for(var i=0;i<this.data.ima.length;i++){
+                    tempURL.push({})
+                    tempURL[i].url=res.fileList[i].tempFileURL
+                    tempURL[i].status="done"
+                    tempImaURL.push({})
+                    tempImaURL[i].url=res.fileList[i].tempFileURL
+                    tempImaURL[i].status="done"
+                }
+                this.setData({ imaURL :tempURL,
+                tempImaURL:tempImaURL});
+                console.log(this.data.tempima)
+                console.log(this.data.ima)
+              },
+              fail: console.error
         })
     },
-
+    afterRead(event) {
+        const upIndex=event.detail.index
+        const file  = event.detail.file;
+        // 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
+        const tempImaURL = this.data.tempImaURL;
+        tempImaURL.push({
+            status:"uploading",
+            url:file.url
+        })
+        this.setData({ tempImaURL });
+        this.uploadImag(file.url,upIndex);
+    },
+    //点击X号
+    deleteImg(event) {
+        // 页面删除（假删除）
+        console.log(event)
+        const delIndex = event.detail.index
+        var tempImaURL = this.data.tempImaURL
+        tempImaURL.splice(delIndex, 1)
+        var tempimg=this.data.tempima
+        tempimg.splice(delIndex,1)
+        this.setData({
+          tempImaURL:tempImaURL,
+          tempima:tempimg
+        })
+        // 云存储删除（真删除）  编辑不删除云
+        // var fileID = ""+this.data.tempima[delIndex];
+        // this.deleteCloudSave(fileID)
+    
+      },
+      deleteCloudSave(fileID) {
+        console.log(fileID)
+        wx.cloud.deleteFile({
+        fileList: [fileID],
+        success: res => {
+          // handle success
+          console.log(res.fileList)
+        },
+        fail: console.error
+        })
+      },
+      //上传到云开发存储中
+    uploadImag(fileURL,upIndex){
+        var that = this
+        wx.cloud.uploadFile({
+            cloudPath:""+new Date().getTime()+'.png',//路径
+            filePath:fileURL, 
+            
+            success:res=>{
+                console.log(this.data.tempima)
+                console.log(res)
+                var tempimg=that.data.tempima;
+                tempimg.push({//保存fileID
+                    fileID:res.fileID
+                })
+                console.log(this.data.tempima)
+                this.setData({
+                    tempima:tempimg
+                })
+                console.log(this.data.tempima)
+                that.addImagePath(res.fileID,upIndex)//获得上传成功后的url路径
+            },
+            fail: console.error
+        })
+    },
+    addImagePath(fileId,upIndex) {
+        var that = this
+        wx.cloud.getTempFileURL({
+          fileList: [fileId],
+          success: res => {
+            var url = res.fileList[0].tempFileURL
+            var imaURL=that.data.tempImaURL
+            imaURL[upIndex].url= url
+            imaURL[upIndex].status= 'done'
+            that.setData({ tempImaURL :imaURL});
+          },
+          fail: console.error
+        })
+      },
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
